c858c6a227be15c5cd28f46427a4222939e8cdb4
pelsallsongsandsermons <pelsallsongsandsermons@pech.co.uk>
2025-12-14 21:31:13 +0000

    Song player UI style changes

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service Worker
  * Handles caching and offline functionality
  */
-const CACHE_NAME = 'v3.1.038';
+const CACHE_NAME = 'v3.1.040';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
