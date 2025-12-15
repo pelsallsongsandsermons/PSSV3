f0103fa26956a22762056256521146dd22fc6c22
pelsallsongsandsermons <pelsallsongsandsermons@pech.co.uk>
2025-12-15 11:45:29 +0000

    UI fixes for iPad, start video automatically on iPad

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service Worker
  * Handles caching and offline functionality
  */
-const CACHE_NAME = 'v3.1.041';
+const CACHE_NAME = 'v3.1.042';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
