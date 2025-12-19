231a3fafc96f31702bd9a2d52a981dd12a07acef
pelsallsongsandsermons <pelsallsongsandsermons@pech.co.uk>
2025-12-19 08:44:02 +0000

    Bump version to test auto reload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service Worker
  * Handles caching and offline functionality
  */
-const CACHE_NAME = 'v3.1.109';
+const CACHE_NAME = 'v3.1.110';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
