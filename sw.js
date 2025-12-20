a102a7ad7ad41ed5ff8c56a4733ed205f31cae2f
pelsallsongsandsermons <pelsallsongsandsermons@pech.co.uk>
2025-12-20 14:11:08 +0000

    darken transcribed text when in light mode

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service Worker
  * Handles caching and offline functionality
  */
-const CACHE_NAME = 'v3.0.027';
+const CACHE_NAME = 'v3.0.028';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
