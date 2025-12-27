301720b566032e9d9a114df0a029fb5228838dfd
pelsallsongsandsermons <pelsallsongsandsermons@pech.co.uk>
2025-12-27 15:43:12 +0000

    Bump version

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service Worker
  * Handles caching and offline functionality
  */
-const CACHE_NAME = 'v3.0.037';
+const CACHE_NAME = 'v3.0.038';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
